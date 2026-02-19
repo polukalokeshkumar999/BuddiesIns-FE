da8062533bffce356e35125ad161a7df8d1f84a1
Lokesh Kumar <Lokesh.Kumar@americold.com>
2026-02-19 22:49:14 +0530

    Java Main

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = sc.nextInt();
+        System.out.println(n);
+        sc.close();
+        
+    }
+}
